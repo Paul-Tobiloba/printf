e36c118a6e7c90d3ef2d1321748fb297c7722e14
Paul Tobiloba <48536008+Toby-Codes@users.noreply.github.com>
2021-08-03 06:30:59 +0100

    fixed printf

--- _printf.c
+++ _printf.c
@@ -1,90 +1,50 @@
 #include "holberton.h"
 
-void cleanup(va_list args, buffer_t *output);
-int run_printf(const char *format, va_list args, buffer_t *output);
-int _printf(const char *format, ...);
-
 /**
- * cleanup - Peforms cleanup operations for _printf.
- * @args: A va_list of arguments provided to _printf.
- * @output: A buffer_t struct.
+ * _printf - prints formatted data to stdout
+ * @format: string that contains the format to print
+ * Return: number of characters written
  */
-void cleanup(va_list args, buffer_t *output)
+int _printf(char *format, ...)
 {
-va_end(args);
-write(1, output->start, output->len);
-free_buffer(output);
-}
+	int written = 0, (*structype)(char *, va_list);
+	char q[3];
+	va_list pa;
 
-/**
- * run_printf - Reads through the format string for _printf.
- * @format: Character string to print - may contain directives.
- * @output: A buffer_t struct containing a buffer.
- * @args: A va_list of arguments.
- *
- * Return: The number of characters stored to output.
- */
-int run_printf(const char *format, va_list args, buffer_t *output)
-{
-int i, wid, prec, ret = 0;
-char tmp;
-unsigned char flags, len;
-unsigned int (*f)(va_list, buffer_t *,
-		    unsigned char, int, int, unsigned char);
-
-for (i = 0; *(format + i); i++)
-{
-len = 0;
-if (*(format + i) == '%')
-{
-tmp = 0;
-flags = handle_flags(format + i + 1, &tmp);
-wid = handle_width(args, format + i + tmp + 1, &tmp);
-prec = handle_precision(args, format + i + tmp + 1,
-				  &tmp);
-len = handle_length(format + i + tmp + 1, &tmp);
-
-f = handle_specifiers(format + i + tmp + 1);
-if (f != NULL)
-{
-i += tmp + 1;
-ret += f(args, output, flags, wid, prec, len);
-continue;
-}
-else if (*(format + i + tmp + 1) == '\0')
-{
-ret = -1;
-break;
-}
-}
-ret += _memcpy(output, (format + i), 1);
-i += (len != 0) ? 1 : 0;
-}
-cleanup(args, output);
-return (ret);
-}
-
-/**
- * _printf - Outputs a formatted string.
- * @format: Character string to print - may contain directives.
- *
- * Return: The number of characters printed.
- */
-int _printf(const char *format, ...)
-{
-buffer_t *output;
-va_list args;
-int ret;
-
-if (format == NULL)
-return (-1);
-output = init_buffer();
-if (output == NULL)
-return (-1);
-
-va_start(args, format);
-
-ret = run_printf(format, args, output);
-
-return (ret);
+	if (format == NULL)
+		return (-1);
+	q[2] = '\0';
+	va_start(pa, format);
+	_putchar(-1);
+	while (format[0])
+	{
+		if (format[0] == '%')
+		{
+			structype = driver(format);
+			if (structype)
+			{
+				q[0] = '%';
+				q[1] = format[1];
+				written += structype(q, pa);
+			}
+			else if (format[1] != '\0')
+			{
+				written += _putchar('%');
+				written += _putchar(format[1]);
+			}
+			else
+			{
+				written += _putchar('%');
+				break;
+			}
+			format += 2;
+		}
+		else
+		{
+			written += _putchar(format[0]);
+			format++;
+		}
+	}
+	_putchar(-2);
+	return (written);
 }
